44c41e007ebca0e6d80c6dd700b4365df7bae7d6
VISHVA G <118836269+Vishvaganesank@users.noreply.github.com>
2025-02-14 16:45:44 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class q1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String name = scan.nextLine();
+        int age = scan.nextInt();
+        System.out.println("My name is " + name);
+        System.out.println("My age is "+ age);
+
+    }
+    
+}
